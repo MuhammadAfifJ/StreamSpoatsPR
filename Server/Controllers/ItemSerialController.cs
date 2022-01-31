c31df84a058a0bbaf1257833bf9b2fd486b20aba
MUHAMMAD 'AFIF BIN MUHD LOTFI JARHOM <2000559A@Student.tp.edu.sg>
2022-01-31 15:17:34 +0800

    All pages without prodname/serial loading

--- Server/Controllers/ItemSerialController.cs
+++ Server/Controllers/ItemSerialController.cs
@@ -42,7 +42,7 @@ namespace StreamSpoatsPR.Server.Controllers
             return serial;
         }
 
-        // PUT: api/Sport/5
+        // PUT: api/Serial/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutSerial(int id, Serial serial)
